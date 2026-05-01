5cc09bf2ca2c2db009e708e238db961293d9c701
XYZ-tec <musferahnasar386@gmail.com>
2026-05-01 20:42:48 +0500

    somethingggg

--- NexOS/apps/snipingtool.cpp
+++
@@ -1,5 +0,0 @@
-// scheduler_vis.cpp — implement using apps/app_template.cpp as base
-#include "../include/theme.h"
-#include "../include/resources.h"
-#include "../include/ipc.h"
-int main() { return 0; }
